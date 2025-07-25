4d4aa04969755198155f3a9b18be1e45c9116b99
An <162073486+AnDayno@users.noreply.github.com>
2025-07-26 07:07:15 +0800

    Updated Timer Script

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,28 +7,50 @@ public class Timer : MonoBehaviour
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectAnswer = 10f;
 
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
+    public float fillFraction;
 
     float timerValue;
 
     void Update()
     {
-        UpdateTimer();
+        UpdateTimer(); 
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;
     }
 
     void UpdateTimer()
     {
         timerValue -= Time.deltaTime;
 
-        if (timerValue <= 0 && !isAnsweringQuestion)
+        if (isAnsweringQuestion)
         {
-            timerValue = timeToCompleteQuestion;
-            isAnsweringQuestion = true;
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                timerValue = timeToShowCorrectAnswer;
+                isAnsweringQuestion = false;
+            }
         }
-        else if (timerValue <= 0 && isAnsweringQuestion)
+        else
         {
-            timerValue = timeToShowCorrectAnswer;
-            isAnsweringQuestion = false;
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
+                timerValue = timeToCompleteQuestion;
+                isAnsweringQuestion = true;
+                loadNextQuestion = true;
+            }
         }
     }
 }
